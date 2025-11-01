270bbee462de7f08063e59294933c957311a5b88
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-11-01 16:15:27 -0400

    duplicate job listing

---
+++ Duplicate_Job_Listings.sql
@@ -0,0 +1,14 @@
+--duplicate job listing = same title and description within same company 
+with cte as (
+select j1.company_id
+from job_listings j1
+inner join job_listings j2
+on j1.company_id = j2.company_id
+and j1.description = j2.description
+and j1.title = j2.title 
+and j1.job_id <> j2.job_id
+group by j1.company_id
+)
+
+select count(company_id) as duplicate_companies
+from cte
