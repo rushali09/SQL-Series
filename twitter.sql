f730fca0c63998dff6e3fa9bdc57c22900d279ea
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-11-13 16:51:26 -0500

    Well paid employees

---
+++ twitter.sql
@@ -0,0 +1,10 @@
+-- employes earning more than their direct managers
+
+SELECT e.employee_id,
+       e.name AS employee_name
+FROM employee e
+JOIN employee m
+    ON e.manager_id = m.employee_id
+WHERE e.salary > m.salary;
+
+
