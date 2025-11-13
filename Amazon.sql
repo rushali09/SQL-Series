eb4b3bcdf758241637a9fa59cf58014edeb341ff
rushali sreedhar <rushalisreedhar37@gmail.com>
2025-11-13 16:16:57 -0500

    Average Review Ratings

---
+++ Amazon.sql
@@ -0,0 +1,8 @@
+SELECT 
+    EXTRACT(MONTH FROM submit_date) AS mth,
+    product_id AS product,
+    ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews
+GROUP BY 
+    EXTRACT(MONTH FROM submit_date),
+    product_id
